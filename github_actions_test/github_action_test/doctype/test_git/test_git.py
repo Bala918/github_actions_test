2b110a8d815195e95646fec68f225ffe5b786213
K.Balakrishnan <110815826+Bala918@users.noreply.github.com>
2024-03-08 10:57:01 +0530

    Update test_git.py

--- github_actions_test/github_action_test/doctype/test_git/test_git.py
+++ github_actions_test/github_action_test/doctype/test_git/test_git.py
@@ -2,4 +2,12 @@ from frappe.model.document import Document
 
 class TestGit(Document):
 	def before_save(self):
-		pass
+		lst = [50, 6, 6, 7,4,33]
+		if not lst:
+			return None
+	    	max_num = lst[0]
+	    	for num in lst:
+			if num > max_num:
+		    		max_num = num
+	    			print(max_num)
+				break;
