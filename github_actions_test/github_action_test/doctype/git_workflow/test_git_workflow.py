81b968e0fb5840a04c329397e7f80a49a690077e
K.Balakrishnan <110815826+Bala918@users.noreply.github.com>
2024-03-08 18:09:13 +0530

    Update test_git_workflow.py

--- github_actions_test/github_action_test/doctype/git_workflow/test_git_workflow.py
+++ github_actions_test/github_action_test/doctype/git_workflow/test_git_workflow.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, Test and Contributors
 # See license.txt
 
-# import frappe
+import frappe
+import unittest
+
 from frappe.tests.utils import FrappeTestCase
 
-
 class TestGitWorkflow(FrappeTestCase):
-	pass
+	def test_sum_of_list(self):
+		a = self.sum_of_list()
+		self.assertEqual(a , 15)	
